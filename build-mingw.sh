353f571219327c767d5d29bc7fb9cb10c5a2ccfe
Ebrahim Byagowi <ebrahim@gnu.org>
2022-09-23 16:55:04 +0330

    Unbreak the Windows build

--- build-mingw.sh
+++ build-mingw.sh
@@ -2,6 +2,5 @@
 
 rm -f visibility.exe
 x86_64-w64-mingw32-gcc -static-libgcc -Wl,-Bstatic -fno-exceptions \
-    -Wall -Werror -o visibility.exe -Ofast \
-    visibility.cc thirdparty/astro_demo_common.c thirdparty/astronomy.c
+    -Wall -Werror -o visibility.exe -O3 visibility.cc thirdparty/astronomy.c
 x86_64-w64-mingw32-strip visibility.exe
