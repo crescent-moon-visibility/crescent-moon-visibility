0bb8097b61e2922aac3328294945e7fb68b66357
hidp123 <84683703+hidp123@users.noreply.github.com>
2022-09-27 11:53:38 +0100

    Update build-mingw.sh

--- build-mingw.sh
+++ build-mingw.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -f visibility.exe
-for i in 1 2 4; do
-    x86_64-w64-mingw32-gcc -static-libgcc -Wl,-Bstatic -fno-exceptions -DPIXEL_PER_DEGREE=$i \
-        -Wall -Werror -o visibility-$i.exe -fopenmp -O3 visibility.cc thirdparty/astronomy.c
-    strip visibility$i.exe
-done
+PIXEL_PER_DEGREE=4
+
+x86_64-w64-mingw32-gcc -static-libgcc -Wl,-Bstatic -fno-exceptions -DPIXEL_PER_DEGREE=$PIXEL_PER_DEGREE \
+    -Wall -Werror -o visibility-$PIXEL_PER_DEGREE.exe -fopenmp -O3 visibility.cc thirdparty/astronomy.c
+strip visibility$PIXEL_PER_DEGREE.exe
