92b7907c7760b7cade3c6b129e0755ddbbf6e0d3
Ebrahim Byagowi <ebrahim@gnu.org>
2022-09-27 09:02:14 +0330

    Build multiple versions of visibility with different output quality

--- build-mingw.sh
+++ build-mingw.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -f visibility.exe
-x86_64-w64-mingw32-gcc -static-libgcc -Wl,-Bstatic -fno-exceptions -DPIXEL_PER_DEGREE=4 \
-    -Wall -Werror -o visibility.exe -fopenmp -O3 visibility.cc thirdparty/astronomy.c
-strip visibility.exe
+for i in 1 2 4; do
+    x86_64-w64-mingw32-gcc -static-libgcc -Wl,-Bstatic -fno-exceptions -DPIXEL_PER_DEGREE=$i \
+        -Wall -Werror -o visibility-$i.exe -fopenmp -O3 visibility.cc thirdparty/astronomy.c
+    strip visibility$i.exe
+done
