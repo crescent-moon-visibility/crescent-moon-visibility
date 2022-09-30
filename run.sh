8c21f2948df9eaeaa24283604b6320008e79bad7
Ebrahim Byagowi <ebrahim@gnu.org>
2022-09-30 10:49:54 +0330

    Improve run.sh script

--- run.sh
+++ run.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 CC=gcc
+OPEN=xdg-open
 if [[ "$OSTYPE" == "darwin"* ]]; then
     CC="/opt/homebrew/opt/llvm/bin/clang -L/opt/homebrew/opt/llvm/lib -fno-rtti" # -mllvm -polly
+    OPEN=open
 fi
 
 rm -f visibility.out
 $CC -fopenmp -O3 -Wall -Werror -o visibility.out -fno-exceptions -DPIXEL_PER_DEGREE=4 visibility.cc thirdparty/astronomy.c -lm || exit $?
 
-#rm -f *.png
-# time ./visibility.out 2022-06-29.png 2022-06-29
-# composite -blend 80 2022-06-29.png map.png 2022-06-29.png
-#time ./visibility.out 2022-08-28.png 2022-08-28
-#time ./visibility.out 2022-08-29.png 2022-08-29
+echo "Compiliation is completed, now let's run the code."
 
 DATE=2022-08-27
 time ./visibility.out $DATE map evening yallop $DATE.png || (echo Not successful && exit 1)
 composite -blend 60 $DATE.png map.png $DATE.png
-#open $DATE.png
+$OPEN $DATE.png
