2d9b8128ee88bc67213163a85ecb7228f6b93417
Ebrahim <ebrahim@gnu.org>
2026-09-24 11:28:51 +0330

    Resolve imagemagick complains

--- run.sh
+++ run.sh
@@ -16,8 +16,16 @@ DATE=${DATE:-2022-08-27}
 TYPE=${TYPE:-evening}
 METHOD=${METHOD:-yallop}
 time ./visibility.out $DATE map $TYPE $METHOD $DATE.png || (echo Not successful && exit 1)
-composite -blend 60 $DATE.png map.png $DATE.png
+
+FONT_ARGS=()
+if [[ "$OSTYPE" == "darwin"* && -f /System/Library/Fonts/Supplemental/Arial.ttf ]]; then
+    FONT_ARGS=(-font /System/Library/Fonts/Supplemental/Arial.ttf)
+elif [[ -f /usr/share/fonts/truetype/dejavu/DejaVuSans.ttf ]]; then
+    FONT_ARGS=(-font /usr/share/fonts/truetype/dejavu/DejaVuSans.ttf)
+fi
+
+magick composite -blend 60 "$DATE.png" map.png "$DATE.png"
 TYPE="$(tr '[:lower:]' '[:upper:]' <<< ${TYPE:0:1})${TYPE:1}"
 METHOD="$(tr '[:lower:]' '[:upper:]' <<< ${METHOD:0:1})${METHOD:1}"
-convert -pointsize 20 -fill black -draw "gravity south text 0,0 '$TYPE, $METHOD, $DATE'" $DATE.png $DATE.png
+magick "$DATE.png" "${FONT_ARGS[@]}" -pointsize 20 -fill black -draw "gravity south text 0,0 '$TYPE, $METHOD, $DATE'" "$DATE.png"
 $OPEN $DATE.png
